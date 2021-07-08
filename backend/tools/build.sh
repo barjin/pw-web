96169cb5c1e279f7779da1bb5377f6f6e356b6c2
unknown <jindrichbar@gmail.com>
2021-07-08 11:30:46 +0200

    [GH Actions] Build script update

--- backend/tools/build.sh
+++ backend/tools/build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 cd ..
+echo "Installing pwww-server dependencies..."
 sudo npm install .
 cd playwright
+echo "Installing playwright dependencies..."
 sudo npm install --production=false .
 cd ..
+echo "Compiling pwww-server ts to js..."
 sudo tsc
 cp ./package.json ./build
+echo "pwww-server has been successfully built to ./backend/build!"
+ls ./build
