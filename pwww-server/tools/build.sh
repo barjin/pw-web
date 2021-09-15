08fc543f88349c0aba021427de3c2b91363f46bc
unknown <jindrichbar@gmail.com>
2021-09-15 11:09:11 +0200

    [GH Actions 15/09] Debugging GH actions

--- pwww-server/tools/build.sh
+++ pwww-server/tools/build.sh
@@ -2,8 +2,7 @@
 cd ..
 echo "Installing pwww-server dependencies..."
 sudo npm install .
-cd ..
 echo "Compiling pwww-server ts to js..."
-sudo tsc
+sudo tsc .
 sudo cp ./package.json ./build/src
 echo "pwww-server has been successfully built!"
