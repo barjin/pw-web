846c7bebefadf7e38fda8a16d09292c932bcafb0
unknown <jindrichbar@gmail.com>
2021-09-15 11:03:46 +0200

    [Daily 15/09c] Github Actions Update

--- pwww-server/tools/build.sh
+++ pwww-server/tools/build.sh
@@ -2,12 +2,8 @@
 cd ..
 echo "Installing pwww-server dependencies..."
 sudo npm install .
-cd playwright
-echo "Installing playwright dependencies..."
-sudo npm install --production=false .
 cd ..
 echo "Compiling pwww-server ts to js..."
 sudo tsc
 sudo cp ./package.json ./build/src
-sudo rm -r ./build/playwright
 echo "pwww-server has been successfully built!"
